d3c195a32e342a1cd2603b94269312d4797cad42
Terria028 <86240437+Terria028@users.noreply.github.com>
2022-04-18 12:51:48 +0800

    Fixed card

--- gen/aclc.js
+++ gen/aclc.js
@@ -71,7 +71,7 @@ const cards = [
             title: "Online Student Enrollment System and Payment System",
             description: "GREAT NEWS! You may now pay your tuition and other fees online. Pay via credit card, online banking, 7-Eleven, Cebuana, SM Bills payment and other safe and convenient payment methods."
         },
-        image: ""
+        image: "assets/card/card1.jpg"
     }),
     new ImageCard({
         card: {
